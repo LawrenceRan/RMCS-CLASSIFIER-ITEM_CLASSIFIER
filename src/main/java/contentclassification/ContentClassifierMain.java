a37e51d473648b2523eaea4df8c0566cae7b0056
rsl_prod_005 <rsl_prod_005@RSL-PROD-005s-MacBook-Pro.local>
2017-02-28 15:43:28 +0000

    optimized get parts of speech by caching results from parts of speech tagging using mongo as data store.

--- src/main/java/contentclassification/ContentClassifierMain.java
+++ src/main/java/contentclassification/ContentClassifierMain.java
@@ -1,10 +1,12 @@
 package contentclassification;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by rsl_prod_005 on 5/6/16.
